40eac3aba66bc08581ec981d25def33fe38fdb0d
KerichKiptoo <dennieskerry@gmail.com>
2022-10-16 01:19:07 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -11,10 +11,6 @@
 
 int main(void)
 {
-/*
- * a function to print,
- * with proper grammar, but the outcome is a piece of art,
- */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
