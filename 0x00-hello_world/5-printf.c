8e74d941b1ce51fc58012f150f7bfeb2e4e0f857
KerichKiptoo <dennieskerry@gmail.com>
2022-10-16 01:12:26 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,21 @@
+/*
+ * File: 5-printf.c
+ *Author: KerichKiptoo
+ */
+
 #include <stdio.h>
-
-
-
-/**
- *
- *   * main - Prints a text
- *
- *     *
- *
- *       * Return: Always 0 (Success)
- *
- *         */
+/*
+ *main - function to print the required output
+ *Return: Always 0.
+ */
 
 int main(void)
-
 {
+/*
+ * a function to print,
+ * with proper grammar, but the outcome is a piece of art,
+ */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	return (0);
 }
