1f96d9bbbac2a61d5bc35b06e0c9cd2f4a2ea838
KerichKiptoo <dennieskerry@gmail.com>
2022-10-17 01:40:53 -0700

    base 10 numerals

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 10 numerals
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <=9; a++)
+
+	{
+		printf("%d", a);
+
+	}
+
+	printf("\n");
+
+	return (0);
+}
