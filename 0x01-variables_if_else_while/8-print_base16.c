b69e78b7584efc796dc6b9429e121c7e86ab93ff
KerichKiptoo <dennieskerry@gmail.com>
2022-10-17 03:06:47 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,19 @@
 int main(void)
 {
 	int i;
+	char a;
 
 	for (i = 0; i <= 15; i++)
 	{
-		putchar(i + '0');
+		putchar((i % 10) + '0');
+
 	}
+
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+
 	putchar('\n');
 
 	return (0);
