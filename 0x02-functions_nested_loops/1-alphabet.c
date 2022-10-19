df7a7b318c220c6ff7c24af27d36db9f6a1ebe11
KerichKiptoo <dennieskerry@gmail.com>
2022-10-19 02:25:07 -0700

    Task 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,8 +9,10 @@ void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
 		_putchar('\n');
-	return (0);
+
+
 }
