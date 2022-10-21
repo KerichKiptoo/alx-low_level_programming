688c3ef7d1a19d153986acaad628f918040f1a75
KerichKiptoo <dennieskerry@gmail.com>
2022-10-21 13:10:20 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,7 +12,11 @@ void print_line(int n)
 {
 	int t;
 
-	if (n > 0)
+	if (n <= 0)
+	{
+		_putchar('\n'); 
+	}
+	else
 	{
 		int t = 0;
 
@@ -22,12 +26,6 @@ void print_line(int n)
 
 			t++;
 		}
-	
-	_putchar('\n');
 	}
 	
-	else
-	{
-		_putchar('\n');
-	}
 }
