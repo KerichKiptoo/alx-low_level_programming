3c77a16502432d9f57f48271f6fef20f25ac7f41
KerichKiptoo <dennieskerry@gmail.com>
2022-10-22 22:17:07 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,12 @@
 void print_most_numbers(void)
 {
 	int i = 0;
+
 	while (i <= 9)
 	{
 		if (i != 2 && i != 4)
-			_putchar(i + '0');
-			i++;
+		_putchar(i + '0');
+		i++;
 	}
 	_putchar('\n');
 }
