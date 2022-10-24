33b797418f8c7259a697d01875aa9ce5b0262e69
KerichKiptoo <dennieskerry@gmail.com>
2022-10-24 11:30:03 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,7 +27,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-		else 
+		else
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
