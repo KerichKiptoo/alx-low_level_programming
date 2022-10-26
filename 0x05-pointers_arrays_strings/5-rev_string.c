419b12e3a403af403929b87803e778daa76004d8
KerichKiptoo <dennieskerry@gmail.com>
2022-10-26 08:11:36 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: input string
+ */
+
+void rev_string(char *s)
+{
+	int len, a;
+
+	for (len = 0; len != '\0'; len++)
+	{
+	}
+	for (a = len - 1; a >= 0; a--)
+	{
+		return (a);
+	}
+}
+
