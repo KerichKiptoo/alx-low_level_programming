87f8f481ed906babec15dd6bb1fce6c06a12f3a7
KerichKiptoo <dennieskerry@gmail.com>
2022-10-27 22:54:47 -0700

    tASK 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,7 @@ void puts_half(char *str)
 	}
 	if (len % 2 == 0)
 	{
-		for (i = (len - 1) / 2; i <= len - 1; i++)
+		for (i = (len) / 2; i <= len - 1; i++)
 		{
 			_putchar(str[i]);
 		}
@@ -22,7 +22,7 @@ void puts_half(char *str)
 
 	else
 	{
-		for (i = (len - 2) / 2; i <= len - 1; i++)
+		for (i = (len - 1) / 2; i <= len - 1; i++)
 		{
 			_putchar(str[i]);
 		}
