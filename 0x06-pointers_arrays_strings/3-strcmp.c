e8ca8a4039d937214dfa40bfd4fa945fb6890dea
KerichKiptoo <dennieskerry@gmail.com>
2022-11-02 01:38:22 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,8 +9,8 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1_len = strlen(*s1);
-	int s2_len = strlen(*s2);
+	int s1_len = strlen(s1);
+	int s2_len = strlen(s2);
 
 	if (s1_len == s2_len)
 		return (0);
