baa52ed8db885b3618c2e7ae31c7db7234110f64
KerichKiptoo <dennieskerry@gmail.com>
2022-11-01 23:20:39 -0700

    Task 0

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,12 +10,12 @@ i
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int src_len = strlen(src);
 	int dest_len = strlen(dest);
+	int i;
 
-	for (i = 0; i < n && i <= src_len; i++)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
-	}
+	dest[dest_len + i] = '\0';
+
 	return (dest);
 }
