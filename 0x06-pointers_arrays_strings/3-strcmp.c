41dee30299b820696aa24b940276651039872bcf
KerichKiptoo <dennieskerry@gmail.com>
2022-11-02 01:26:27 -0700

    Task 0

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1_len = strlen(s1);
-	int s2_len = strlen(s2);
+	int s1_len = strlen(*s1);
+	int s2_len = strlen(*s2);
 
-	if (s1 == s2)
+	if (s1_len == s2_len)
 		return (0);
-	else if (s1 < s2)
-		return (s1 - s2);
+	else if (s1_len < s2_len)
+		return (s1_len - s2_len);
 	else
-		return (s2 - s1);
+		return (s2_len - s1_len);
 }
 
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements to swap
+ */
+
+void reverse_array(int *a, int n)
+{
+
