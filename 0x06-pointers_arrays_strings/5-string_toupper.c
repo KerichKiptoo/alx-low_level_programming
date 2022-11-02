ccb3f081bd510100428152a888305d476eca2503
KerichKiptoo <dennieskerry@gmail.com>
2022-11-02 11:50:49 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#inlcude "main.h"
+/**
+ * string_toupper -  changes all lowercase letters of a string to uppercase
+ * @n: Character to be upper cased
+ * Return: upper case character
+ */
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
