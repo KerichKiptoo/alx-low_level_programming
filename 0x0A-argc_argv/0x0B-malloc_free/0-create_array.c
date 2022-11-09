da1d9b10a468826f407c64511ec20e4f9a2f2d57
KerichKiptoo <dennieskerry@gmail.com>
2022-11-09 01:43:15 -0800

    Task 0

---
+++ 0x0A-argc_argv/0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * create_array - creates an array of chars,
+ * initializes it with a specific char
+ * @size: size of the array.
+ * @c: char to initialize the array c
+ *
+ * Return: pointer to the array (Success), NULL (Error)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *cr;
+
+	insigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		cr[i] = c;
+
+	return (cr);
+}
